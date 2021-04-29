e3742b5be53df3362a1be81a177000a6fc2e456f
mazoth <mkluften93@gmail.com>
2021-04-29 18:57:14 +0200

    Set up graphql source schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,39 @@ module.exports = {
   siteMetadata: {
     title: "Dr Dropin",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-styled-components", 
+    "gatsby-plugin-react-helmet", 
+    `gatsby-plugin-typescript`,
+    {
+      resolve: 'gatsby-source-custom-api',
+      options: {
+        url: "https://staging-core.api.drdropin.no/v1/clinics"
+      },
+      rootKey: 'customApi',
+      schemas: {
+        customApi: `
+          name: String!
+          openingHours: openingHours
+        `,
+        openingHours: `
+          mon: dayInfo
+          tue: dayInfo
+          wed: dayInfo
+          thu: dayInfo
+          fri: dayInfo
+          sat: dayInfo
+          sun: dayInfo
+        `,
+        dayInfo: `
+          periods: [period]
+          isOpen: Boolean
+        `,
+        period: `
+          to: Int
+          from: Int
+        `,
+      }
+    }
+  ],
 };
